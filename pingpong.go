b70592e19dac6d5ef2e6d4de9c1b9a3d6bea31d0
Praneetha Babburi <praneethababburi@Praneethas-MacBook-Air.local>
2026-03-31 11:10:52 -0700

    without using for loop

--- pingpong.go
+++ pingpong.go
@@ -1,43 +1,53 @@
 package main
 
 import (
-	"fmt"
-	"sync"
-	"time"
+    "fmt"
+    "sync"
+    "time"
 )
 
 func main() {
-	ping := make(chan string)
-	pong := make(chan string)
-	var wg sync.WaitGroup
-	iterations := 5
-	wg.Add(2)
-	go func(iterations int) {
-		for i := 0; i < iterations; i++ {
-			pingmsg := <-ping
-			fmt.Println(pingmsg)
-			pong <- "pong"
-		}
-		wg.Done()
-	}(iterations)
+    ping := make(chan string)
+    pong := make(chan string)
+    var wg sync.WaitGroup
+    iterations := 5
+    count:=0
+    wg.Add(2)
+    go pingfunc(&count,ping, pong, &wg,iterations)
+    go pongfunc(&count,ping, pong, &wg,iterations)
 
-	go func(iterations int) {
+    time.Sleep(time.Second)
+    ping <- "ping"
+    
+    wg.Wait()
 
-		for i := 0; i < iterations; i++ {
-			pongmsg := <-pong
-			fmt.Println(pongmsg)
-			if i < iterations-1 {
-				ping <- "ping"
-			}
-		}
+}
+func pingfunc(count *int,ping,pong chan string,wg *sync.WaitGroup,iterations int) {
+    msg, ok := <-ping
+        if !ok {
+            wg.Done()
+            return
+        }
 
-		wg.Done()
-	}(iterations)
-
-
-	time.Sleep(time.Second)
-	ping <- "ping"
-	
-	wg.Wait()
-
+        fmt.Println(msg)
+        pong <- "pong"
+        
+        pingfunc(count,ping, pong, wg,iterations)
+        
+    
+}
+func pongfunc(count *int,ping,pong chan string,wg *sync.WaitGroup,iterations int) {
+    pongmsg := <-pong
+    fmt.Println(pongmsg)
+    (*count)++
+    if *count>=iterations{
+        close(ping)
+        wg.Done()
+        return  
+    }
+    ping <- "ping"
+    pongfunc(count,ping, pong, wg,iterations)
+            
+        
+    
 }
